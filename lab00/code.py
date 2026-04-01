fb1a8f86880c6f56aacaf6a82e735c7246b76aa7
TagoreZhao <zhaotagore@gmail.com>
2026-04-01 11:22:40 -0700

    update

--- lab00/code.py
+++ lab00/code.py
@@ -7,17 +7,9 @@ def get_airspeed_velocity_of(unladen_swallow):
 # pretend there's code here...
 
 def fizzbuzz(num):
-<<<<<<< HEAD
   if (num % 3 == 0) & (num % 5 == 0): # edit this line
     print(f"{num}: fizznum")
   elif (num % 5) == 0: # edit this line
-=======
-  if str(num) in ["15"]:
-    print(f"{num}: fizzbuzz")
-  elif str(num) in ["3", "6", "9", "12", "15", "18"]:
-    print(f"{num}: fizz")
-  elif str(num) in ["5", "10", "15"]:
->>>>>>> 0bdba63993cd46d120b74cf550d3d551c99b32f3
     print(f"{num}: buzz")
   elif (num % 3) == 0:
     print(f"{num}: fizz")
